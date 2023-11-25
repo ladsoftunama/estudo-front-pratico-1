4dacbab304f4d2ac838ff456ad5b559816a7a90b
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-11-24 23:19:44 -0300

    desafio 1

---
+++ src/components/topbar/component_topBar.js
@@ -0,0 +1,10 @@
+import style from "./style.module.css";
+export default function TopBar() {
+  return (
+    <>
+      <div className={style.container1}>
+        <h1>ola mundo</h1>
+      </div>
+    </>
+  );
+}
